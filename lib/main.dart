2e6603d1f98c0e08fc43655dd3834b15fbcf5b08
LoganAnde <posttrauma.logan@gmail.com>
2021-02-15 19:58:17 -0600

    add counterdemo screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lesson0/screen/counterdemo_screen.dart';
+import 'package:lesson0/screen/start_screen.dart';
 
 void main() {
   runApp(Lesson2App());
@@ -8,7 +10,11 @@ class Lesson2App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Lesson2App'),
+      initialRoute: StartScreen.routeName,
+      routes: {
+        StartScreen.routeName: (context) => StartScreen(),
+        CounterDemoScreen.routeName: (context) => CounterDemoScreen(),
+      },
     );
   }
 }
---
+++ lib/screen/start_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lesson0/screen/counterdemo_screen.dart';
+
+class StartScreen extends StatelessWidget {
+  static const routeName = '/startscreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose a menu'),
+      ),
+      body: Column(
+        children: [
+          RaisedButton(
+            onPressed: () => Navigator.pushNamed(context, CounterDemoScreen.routeName),
+            child: Text(
+              'Counter Demo',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          RaisedButton(
+            onPressed: null,
+            child: Text('Menu 2'),
+          ),
+        ],
+      ),
+    );
+  }
+}
