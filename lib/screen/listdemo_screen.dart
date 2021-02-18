24fbade43a644272d999d3c700a55a4fe00eecaa
LoganAnde <posttrauma.logan@gmail.com>
2021-02-18 09:00:58 -0600

    modify stylings on list demo

--- lib/screen/listdemo_screen.dart
+++ lib/screen/listdemo_screen.dart
@@ -64,12 +64,18 @@ class _Controller {
                 ),
                 Expanded(
                   flex: 1,
-                  child: Text(course.number),
+                  child: Text(course.number, style: Theme.of(state.context).textTheme.headline5),
                 ),
               ],
             ),
-            Text(course.title),
-            Text('Prereq: ${course.prereq}'),
+            Container(
+              padding: EdgeInsets.only(left: 15.0),
+              child: Text(course.title, style: Theme.of(state.context).textTheme.headline6),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15.0, bottom: 10.0),
+              child: Text('Prereq: ${course.prereq}', style: Theme.of(state.context).textTheme.headline6),
+            ),
           ],
         ),
       );
