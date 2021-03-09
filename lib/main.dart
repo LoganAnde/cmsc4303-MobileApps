dc97f7c6ccc604d0b3564fed4454a0ec6617ad7f
LoganAnde <posttrauma.logan@gmail.com>
2021-03-08 18:00:14 -0600

    set up lesson3 app

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:lesson0/screen/counterdemo_screen.dart';
-import 'package:lesson0/screen/formdemo_screen.dart';
-import 'package:lesson0/screen/listdemo_screen.dart';
-import 'package:lesson0/screen/listevent_screen.dart';
-import 'package:lesson0/screen/profile_screen.dart';
-import 'package:lesson0/screen/start_screen.dart';
-import 'package:lesson0/screen/userhome_screen.dart';
-import 'package:lesson0/screen/widgetlifecycle_screen.dart';
+import 'package:lesson0/screen/signin_screen.dart';
 
 void main() {
-  runApp(Lesson2App());
+  runApp(PhotoMemoApp());
 }
 
-class Lesson2App extends StatelessWidget {
+class PhotoMemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: StartScreen.routeName,
+      initialRoute: SignInScreen.routeName,
       routes: {
-        StartScreen.routeName: (context) => StartScreen(),
-        CounterDemoScreen.routeName: (context) => CounterDemoScreen(),
-        WidgetLifeCycleScreen.routeName: (context) => WidgetLifeCycleScreen(),
-        ListDemoScreen.routeName: (context) => ListDemoScreen(),
-        ListEventScreen.routeName: (context) => ListEventScreen(),
-        FormDemoScreen.routeName: (context) => FormDemoScreen(),
-        UserHomeScreen.routeName: (context) => UserHomeScreen(),
-        ProfileScreen.routeName: (context) => ProfileScreen(),
+        SignInScreen.routeName: (context) => SignInScreen(),
       },
     );
   }
