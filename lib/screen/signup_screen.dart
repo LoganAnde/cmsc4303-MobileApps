fb8f410425cfeb22fc7a61c5cb1216446a13b90d
LoganAnde <posttrauma.logan@gmail.com>
2021-03-16 18:34:25 -0500

    add signup form

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -10,6 +10,7 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpState extends State<SignUpScreen> {
   _Controller con;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -25,7 +26,47 @@ class _SignUpState extends State<SignUpScreen> {
       appBar: AppBar(
         title: Text('Create an account'),
       ),
-      body: Text('sign up'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Create an account', style: Theme.of(context).textTheme.headline5),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: null,
+                  onSaved: null,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Password'),
+                  obscureText: true,
+                  autocorrect: false,
+                  validator: null,
+                  onSaved: null,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Password confirm'),
+                  obscureText: true,
+                  autocorrect: false,
+                  validator: null,
+                  onSaved: null,
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: Text(
+                    'Create',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
