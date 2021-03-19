762f7ab246e69c91ebf1f7ec3d28f0c47510e0bb
LoganAnde <posttrauma.logan@gmail.com>
2021-03-18 21:24:32 -0500

    add action buttons

--- lib/screen/userhome_screen.dart
+++ lib/screen/userhome_screen.dart
@@ -41,6 +41,10 @@ class _UserHomeState extends State<UserHomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('User Home'),
+          actions: [
+            con.delIndex != null ? IconButton(icon: Icon(Icons.cancel), onPressed: con.cancelDelete) : SizedBox(width: 1.0),
+            con.delIndex != null ? IconButton(icon: Icon(Icons.delete), onPressed: con.delete) : SizedBox(width: 1.0),
+          ],
         ),
         drawer: Drawer(
           child: ListView(
@@ -125,6 +129,7 @@ class _Controller {
   }
 
   void onTap(int index) async {
+    if (delIndex != null) return;
     await Navigator.pushNamed(
       state.context,
       DetailedViewScreen.routeName,
@@ -150,6 +155,13 @@ class _Controller {
   }
 
   void onLongPress(int index) {
+    if (delIndex != null) return;
     state.render(() => delIndex = index);
   }
+
+  void cancelDelete() {
+    state.render(() => delIndex = null);
+  }
+
+  void delete() {}
 }
