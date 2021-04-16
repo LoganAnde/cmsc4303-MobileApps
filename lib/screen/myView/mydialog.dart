558dab22932284d9437d9226871e5926bc5ea092
LoganAnde <posttrauma.logan@gmail.com>
2021-04-15 21:20:59 -0500

    add custom two option dialog

--- lib/screen/myView/mydialog.dart
+++ lib/screen/myView/mydialog.dart
@@ -40,4 +40,39 @@ class MyDialog {
       ),
     );
   }
+
+  static Future<String> twoOptions({
+    @required BuildContext context,
+    @required String content,
+    @required String option1,
+    @required String option2,
+  }) async {
+    String selected;
+
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        content: Text(content),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              selected = option1;
+              Navigator.of(context).pop();
+            },
+            child: Text(option1, style: Theme.of(context).textTheme.button),
+          ),
+          FlatButton(
+            onPressed: () {
+              selected = option2;
+              Navigator.of(context).pop();
+            },
+            child: Text(option2, style: Theme.of(context).textTheme.button),
+          ),
+        ],
+      ),
+    );
+
+    return selected;
+  }
 }
